375c1699b75eec571ed5cca534d7cb6eb46fbc30
Skrodo <163410830+Skrodo@users.noreply.github.com>
2025-02-17 12:29:17 +0100

    Hilfebuttonzeug

---
+++ app/src/main/java/de/sam/abinopolynew/func/Help.kt
@@ -0,0 +1,21 @@
+package de.sam.abinopolynew.func
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import de.sam.abinopolynew.R
+
+class Help : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_help)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
